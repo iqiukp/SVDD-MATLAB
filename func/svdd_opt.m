14db1c227475664391774ce9de8025d02f4433f6
iqiukp <44639227+iqiukp@users.noreply.github.com>
2019-05-24 18:37:47 +0800

    Create svdd_opt.m

---
+++ func/svdd_opt.m
@@ -0,0 +1,47 @@
+% DESCRIPTION
+% Quadratic optimizer for SVDD 
+%
+%    alf = svdd_opt(K,X,C)
+%
+% INPUT
+%   K         Kernel matrix
+%   X         Training data
+%   C         Trade-off parameter
+%
+% OUTPUT
+%   alf       Lagrange multipliers of support vetors
+
+% Created by Kepeng Qiu on May 24, 2019.
+%-------------------------------------------------------------%
+
+function alf = svdd_opt(K,X,C)
+
+%
+N = size(X,1);
+
+% Symmetric Hessian
+labx = ones(N,1);
+D = (labx*labx').*K;
+D = (D+D')/2;
+f = labx.*diag(D);
+
+% Equality constraints
+A = labx';
+b = 1.0;
+
+% Lower and upper bounds
+lb = zeros(N,1);
+ub = ones(N,1)*C(1);
+
+% Initialization
+p = 0.5*rand(N,1);
+
+% Quadratic optimize
+opt = optimset('quadprog'); 
+opt.Algorithm = 'interior-point-convex';
+opt.Display='off';
+alf = quadprog(2.0*D,-f,[],[],A,b,lb,ub,p,opt);
+
+end
+
+
