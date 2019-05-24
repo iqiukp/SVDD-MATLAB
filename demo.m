06f64c11f255c6cc571038f649eaa759a39ccf75
iqiukp <44639227+iqiukp@users.noreply.github.com>
2019-05-24 18:49:24 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -13,17 +13,17 @@ load ('.\data\X.mat')
 load ('.\data\Y.mat')
 
 % Normalization (in general, this step is important for fault detection)
-[X,Y] = normalize(X,Y);
+[X_s,Y_s] = normalize(X,Y);
 
 % Set parameters 
 C = 0.2;   % trade-off parameter
 ker = struct('type','gauss','width',6);
 
 % Train SVDD hypersphere
-model = svdd_train(X,C,ker);
+model = svdd_train(X_s,C,ker);
 
 % Test a new sample Y (vector of matrix)
-d = svdd_test(model,Y);
+d = svdd_test(model,Y_s);
 
 % Plot the results
 plotResult(model.threshold,d)
